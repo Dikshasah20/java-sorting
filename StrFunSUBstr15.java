83e9949af9ab8feee220b8524363a10b77eb1dc0
name <dikshasah761@gmail.com>
2024-10-05 18:11:06 +0530

    printLar

---
+++ StrFunSUBstr15.java
@@ -0,0 +1,15 @@
+public class StrFunSUBstr15 {
+    public static String substring(String str, int si, int ei){
+        String substr = "";
+        for(int i =si; i<ei; i++){
+            substr += str.charAt(i);//concatenate with charector
+        }
+        return substr;
+
+    }
+    public static void main(String[] args) {
+        //Substring
+        String str = "HelloWorld";
+        System.out.println(substring(str, 0, 5));// Hello
+    }
+}
--- stringShortpath13.java
+++ stringShortpath13.java
@@ -1,3 +1,32 @@
 public class stringShortpath13 {
+    public static float getShortestPath(String path){
+        int x = 0, y=0;
+        for(int i=0; i<path.length();i++){
+            char dir = path.charAt(i);
+            //south
+            if(dir == 'S'){
+                y--;
+            }
+            //North
+            else if(dir == 'N'){
+                y++;
+            }//south
+            else if(dir == 'W'){
+                x--;
+            }//East
+            else{
+                x++;
+            }
+        }
+        int X2 = x*x;//(x2-x1)^2 //(x-0)
+        int Y2 = y*y;
+        return (float)Math.sqrt(X2 + Y2);
+
+
+    }
+    public static void main(String args[]){
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }
     
 }
