41e6cbbc9135f2e375a43134213978d09355834f
name <dikshasah761@gmail.com>
2024-10-16 17:42:12 +0530

    shallow

---
+++ shallowDeep25.java
@@ -0,0 +1,20 @@
+public class shallowDeep25 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+    }
+}
+class Student{
+    String name;
+    int roll;
+    String password;
+    int marks[];
+
+// shallow copy constructor
+Student(Student s1){
+    marks = new int[3];
+    this.name = s1.name;
+    this.roll =s1.roll;
+
+     }
+}
