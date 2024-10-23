012447597e8df48a1cdba175e96aaf55d662dfbb
name <dikshasah761@gmail.com>
2024-10-23 13:44:52 +0530

    31

---
+++ interface31.java
@@ -0,0 +1,30 @@
+public class interface31 {
+    public static void main(String[] args) {
+        Queen q = new Queen();
+        q.moves();
+    }
+}
+
+interface ChessPlayer{
+    void moves();// blueprint
+}
+
+class Queen implements ChessPlayer{// classes khud se define karegi moves
+    public void moves(){
+        System.out.println("up, down, left, right, diagonal(in all 4 direction)");
+    }
+}
+
+class Rook implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right");
+    }
+}
+
+
+class King implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right, diagonal(by 1 step)");
+    }
+}
+
