7999974d0a22fd2f4c8fadaeada971b0f01b3293
name <dikshasah761@gmail.com>
2024-11-03 09:37:06 +0530

    bit

---
+++ BitManipulation35.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class BitManipulation35 {
+    public static void main(String[] args) {
+        System.out.println((5&6));
+        System.out.println((5|6));
+    }
+    
+}
