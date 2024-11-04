17a3c6a53a5f9be2988353de04cc17b76f73e7eb
name <dikshasah761@gmail.com>
2024-11-04 09:36:12 +0530

    left

--- BitManipulation35.java
+++ BitManipulation35.java
@@ -3,6 +3,7 @@ public class BitManipulation35 {
     public static void main(String[] args) {
         System.out.println((5&6));
         System.out.println((5|6));
+        System.out.println(5<<2);
     }
     
 }
