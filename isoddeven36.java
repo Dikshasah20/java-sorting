b9d4306aaf4a21ca3b3efbb5f610dac932ee3974
name <dikshasah761@gmail.com>
2024-11-05 10:54:18 +0530

    isoddeven

---
+++ isoddeven36.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class isoddeven36 {
+    public static void oddorEven(int n){
+        int bitMask =1;
+        if((n & bitMask) == 0){
+            System.out.println(n + " "+"is even");
+        }else{
+            System.out.println(n + " "+ "is odd");
+        }
+    }
+    public static void main (String args[]){
+        oddorEven(3);
+        oddorEven(10);
+        oddorEven(33);
+    }
+    
+}
+
